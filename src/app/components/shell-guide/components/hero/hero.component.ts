08a68eecc1b9b2961fd03876769c8e845599539f
Diana Radu <Diana.Horincar@msg.group>
2026-05-11 16:23:30 +0200

    hero comp changes

--- src/app/components/shell-guide/components/hero/hero.component.ts
+++ src/app/components/shell-guide/components/hero/hero.component.ts
@@ -1,12 +1,19 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Output, inject } from '@angular/core';
+
+import { AuthService } from '../../../../services/auth.service';
 
 @Component({
   selector: 'app-hero',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './hero.component.html',
   styleUrl: './hero.component.scss'
 })
 export class HeroComponent {
+  private authService = inject(AuthService);
+
+  displayName$ = this.authService.displayName$;
+
   @Output() scrollToGuide = new EventEmitter<void>();
 }
